3d17ace6dcf07225b21d344219b3e412c862d59c
Mahfuz <sayhello.mahfuz@gmail.com>
2024-05-14 18:47:54 +0600

    delete vercel livel

--- AlumniFinder/settings.py
+++ AlumniFinder/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8a5zh^4op6s5qo)1jov4a_6a37&env1@_j)!c8^^$rtdesxbsv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -116,9 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = ''
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR,""
+    BASE_DIR,"static"
 ]
 
 # Default primary key field type
