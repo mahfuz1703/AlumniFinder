71999a77158c848772625ea835dafc280b4aab3b
Mahfuz <sayhello.mahfuz@gmail.com>
2024-05-20 04:22:01 +0600

    add authentication system

--- AlumniFinder/info.py
+++ AlumniFinder/info.py
@@ -1,5 +1,5 @@
 EMAIL_USER_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'cse.mahfuz17@gmail.com'
-EMAIL_HOST_PASSWORD = 'Mahfuzmia1703@#%058@'
+EMAIL_HOST_PASSWORD = 'pass'
 EMAIL_PORT = 587
